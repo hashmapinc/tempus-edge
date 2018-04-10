3174b018db369d7b48dc18352e73460eb273662e
Randy Pitcher <randypitcherii@gmail.com>
2018-04-09 19:31:10 -0500

    implmented logic + testing for go mqtt client config module

--- applications/mqtt-client-go/src/com/hashmapinc/tempus/edge/mqtt/client/config.go
+++ applications/mqtt-client-go/src/com/hashmapinc/tempus/edge/mqtt/client/config.go
@@ -7,9 +7,12 @@ package client
 
 import (
 	pb "com/hashmapinc/tempus/edge/proto"
-	"errors"
+	"io/ioutil"
 	"log"
 	"os"
+	"sync"
+
+	"github.com/golang/protobuf/proto"
 )
 
 // configure logger
@@ -18,27 +21,39 @@ var logger = log.New(os.Stderr, "", log.LstdFlags|log.LUTC|log.Lshortfile)
 // LocalTrackConfig - local config for this edge application
 var LocalTrackConfig = &pb.TrackConfig{}
 
+// TrackConfigWriteLock ensures the local track config is threadsafe
+var TrackConfigWriteLock = &sync.Mutex{}
+
 // pathToTrackConfig holds the expected location of production configs
 const pathToTrackConfig = "/mnt/config/config.pb"
 
 // UpdateTrackConfig attempts to update LocalTrackConfig with the latest track config
 func UpdateTrackConfig() error {
-	newConfig, err := loadTrackConfig(pathToTrackConfig)
-	if err == nil {
-		*LocalTrackConfig = newConfig
-	}
+	err := loadTrackConfig(pathToTrackConfig, LocalTrackConfig)
 	return err
 }
 
 /* loadTrackConfig loads the config.pb file at path and stores it in tc.
 @param path - string containing the full path to the config.pb file to load
+@param tc - pb.TrackConfig pointer to load track config into
 
-@return tc - pb.TrackConfig loaded from path
 @return err - error, if any, from loading.
 */
-func loadTrackConfig(path string) (tc pb.TrackConfig, err error) {
-	// TODO: implement this for real
-	tc.TrackMetadata = &pb.TrackMetadata{TrackId: 1, TrackName: "new_track"}
-	err = errors.New("Error in loadTrackConfig")
+func loadTrackConfig(path string, tc *pb.TrackConfig) (err error) {
+	// lock and defer unlock
+	TrackConfigWriteLock.Lock()
+	defer TrackConfigWriteLock.Unlock()
+
+	// read file
+	in, err := ioutil.ReadFile(path)
+	if err != nil {
+		logger.Println("File at path", path, "could not be read")
+		return
+	}
+
+	// unmarshal (deserialize) track config
+	if err = proto.Unmarshal(in, tc); err != nil {
+		logger.Println("Failed to parse track config:", err.Error())
+	}
 	return
 }
--- applications/mqtt-client-go/src/com/hashmapinc/tempus/edge/mqtt/client/iofog_controller.go
+++ applications/mqtt-client-go/src/com/hashmapinc/tempus/edge/mqtt/client/iofog_controller.go
@@ -2,12 +2,16 @@ package client
 
 import (
 	"com/hashmapinc/tempus/edge/message"
+	"errors"
 
 	sdk "github.com/iotracks/container-sdk-go"
 )
 
 // OnIofogMessage processes incoming iofog messages
 func OnIofogMessage(msg *sdk.IoMessage) error {
+	if msg == nil || msg.ContentData == nil || len(msg.ContentData) < 2 {
+		return errors.New("received non-tempus-edge message")
+	}
 	payload := msg.ContentData
 	var ptclB = payload[0]
 	var typB = payload[1]

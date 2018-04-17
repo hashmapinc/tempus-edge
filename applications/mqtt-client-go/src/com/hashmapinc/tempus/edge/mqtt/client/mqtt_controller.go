5e1816103327b0e961e7ef2f33ea165658c662eb
Randy Pitcher <randypitcherii@gmail.com>
2018-04-17 17:54:55 -0500

    implemented mqtt controller logic in mqtt go client

--- applications/mqtt-client-go/src/com/hashmapinc/tempus/edge/mqtt/client/mqtt_controller.go
+++ applications/mqtt-client-go/src/com/hashmapinc/tempus/edge/mqtt/client/mqtt_controller.go
@@ -1,9 +1,49 @@
 package client
 
-import paho "github.com/eclipse/paho.mqtt.golang"
+import (
+	"com/hashmapinc/tempus/edge/iofog"
+	pb "com/hashmapinc/tempus/edge/proto"
+
+	paho "github.com/eclipse/paho.mqtt.golang"
+	"github.com/golang/protobuf/proto"
+)
 
 // OnMqttMessage handles new incoming mqtt messages
 func OnMqttMessage(mqttClient paho.Client, msg paho.Message) {
-	// TODO: do this for real
 	logger.Printf("received mqtt msg: %v \n", msg)
+
+	// get qos
+	var qos pb.MqttMessage_QoS
+	switch msg.Qos() {
+	case 0:
+		qos = pb.MqttMessage_ZERO
+	case 1:
+		qos = pb.MqttMessage_ONE
+	case 2:
+		qos = pb.MqttMessage_TWO
+	}
+
+	// create tempus edge MQTT msg
+	mqttMsg := &pb.MqttMessage{
+		Topic:   msg.Topic(),
+		Qos:     qos,
+		Payload: msg.Payload(),
+	}
+
+	// get proto byte array
+	iofogPayload, err := proto.Marshal(mqttMsg)
+	if err != nil {
+		logger.Println("recieved error when marshalling mqtt message: ", err.Error())
+		return
+	}
+
+	// append tempus edge message protocol and type
+	header := []byte{byte(pb.MessageProtocols_DATA), byte(pb.DataMessageTypes_MQTT)}
+	iofogPayload = append(header, iofogPayload...)
+
+	// send payload
+	err = iofog.SendWSMessage(iofogPayload, iofog.Client)
+	if err != nil {
+		logger.Println("recieved error when sending iofog message: ", err.Error())
+	}
 }
